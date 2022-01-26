94e6c52cc41b5c6a508b425e6d4c00b4e0924d7e
Simon <simonbesteiro@gmail.com>
2022-01-26 16:37:50 +0100

    red

--- src/test/java/example/TicTacToeGameShould.java
+++ src/test/java/example/TicTacToeGameShould.java
@@ -63,4 +63,29 @@ class TicTacToeGameShould {
 				result
 		);
 	}
+
+	@Test void
+	mark_the_first_move_in_middle_left_cell_with_an_X() {
+		var game = new TicTacToeGame();
+		var result = game.play(new Point(0, 1));
+		assertEquals(
+				"_|_|_\n" +
+						"X|_|_\n" +
+						"_|_|_",
+				result
+		);
+	}
+
+	@Test void
+	mark_the_second_move_in_bottom_left_cell_with_an_O() {
+		var game = new TicTacToeGame();
+		game.play(new Point(0, 1));
+		var result = game.play(new Point(0, 2));
+		assertEquals(
+				"_|_|_\n" +
+						"X|_|_\n" +
+						"O|_|_",
+				result
+		);
+	}
 }

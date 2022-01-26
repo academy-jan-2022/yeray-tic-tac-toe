f8ee0a50ff9563f81f272f4c3b1f5af59e92232d
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-26 15:03:41 +0100

    refactor

--- src/main/java/example/TicTacToeGame.java
+++ src/main/java/example/TicTacToeGame.java
@@ -2,17 +2,30 @@ package example;
 
 public class TicTacToeGame {
 	public String play(int x, int y) {
+		var line = new StringBuilder();
+		for (int indexX = 0; indexX < 3; indexX++) {
+			if (x == indexX) {
+				line.append("X");
+			} else {
+				line.append("_");
+			}
+			if (indexX < 2)
+				line.append("|");
+		}
+		line.append("\n");
+
+
 		if (x == 1)
-			return "_|X|_\n" +
+			return line +
 					"_|_|_\n" +
 					"_|_|_";
 
 		if (y == 1)
 			return "_|_|_\n" +
-					"X|_|_\n" +
+					line +
 					"_|_|_";
 
-		return "X|_|_\n" +
+		return line +
 				"_|_|_\n" +
 				"_|_|_";
 	}

be929f18d73cd6594be0dcae2693925265fef761
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-26 15:20:57 +0100

    ref

--- src/main/java/example/TicTacToeGame.java
+++ src/main/java/example/TicTacToeGame.java
@@ -2,13 +2,7 @@ package example;
 
 public class TicTacToeGame {
 	public String play(Point point) {
-		var line = new StringBuilder();
-		for (int indexX = 0; indexX < 3; indexX++) {
-			line.append(getCellContent(point, indexX));
-			line.append(getSeparator(indexX));
-		}
-		line.append("\n");
-
+		var line = getLine(point);
 
 		if (point.getX() == 1)
 			return line +
@@ -23,6 +17,16 @@ public class TicTacToeGame {
 		return line +
 				"_|_|_\n" +
 				"_|_|_";
+	}
+
+	private String getLine(Point point) {
+		var line = new StringBuilder();
+		for (int indexX = 0; indexX < 3; indexX++) {
+			line.append(getCellContent(point, indexX));
+			line.append(getSeparator(indexX));
+		}
+		line.append("\n");
+		return line.toString();
 	}
 
 	private String getSeparator(int indexX) {

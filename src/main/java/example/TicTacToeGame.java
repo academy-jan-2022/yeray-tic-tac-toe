c975de3a40a447ceb3351a7e1022a7e951291f5d
Simon <simonbesteiro@gmail.com>
2022-01-27 16:44:06 +0100

    refactor

--- src/main/java/example/TicTacToeGame.java
+++ src/main/java/example/TicTacToeGame.java
@@ -6,10 +6,11 @@ public class TicTacToeGame {
 
 	public String play(Point point) {
 
-		boolean checker= board.setMyCell(getSymbol(), point.getX(), point.getY());
+		boolean isValidMove= board.setMyCell(getSymbol(), point.getX(), point.getY());
 
-		String output = isAnyWinnerCombination(board,getSymbol().charAt(0));
-		if(checker){
+		String output = isGameEnded(board,getSymbol().charAt(0));
+
+		if(isValidMove){
 			player.flipPlayer();
 		}
 		return output;
@@ -23,34 +24,48 @@ public class TicTacToeGame {
 		return "O";
 	}
 
-	private String isAnyWinnerCombination(Board board, char playerSymbol) {
-		if (board.showBoard().contains(""+playerSymbol+"|"+playerSymbol+"|"+playerSymbol)){
-			return "Player "+playerSymbol+" wins";
-		}
-
+	private String isGameEnded(Board board, char playerSymbol) {
 		String result = board.showBoard().replace("|", "").replace("\n", "");
+		if(     isARowWinning(board, playerSymbol) ||
+				isAColumnWinning(playerSymbol, result) ||
+				isADiagonalWinning(playerSymbol, result)
+		){
+				return "Player " + playerSymbol + " wins";
+		};
+		if (isItADraw(board)) {
+			return "Draw";
+		}
+		return board.showBoard();
+	}
+
+
+
+	private Boolean isARowWinning(Board board, char playerSymbol) {
+		return board.showBoard().contains(playerSymbol +"|"+ playerSymbol +"|"+ playerSymbol);
+	}
+
+	private  Boolean isAColumnWinning(char playerSymbol, String result){
 
 		for (var i = 0; i < 3; i++){
 			if (result.charAt(i) == playerSymbol
 					&& result.charAt(3+i) == playerSymbol
 					&& result.charAt(6+i) == playerSymbol){
-				return "Player "+playerSymbol+" wins";
+				return true;
 			}
 		}
+		return false;
+	}
 
+	private  Boolean isADiagonalWinning(char playerSymbol, String result ){
 		if(result.charAt(0) == playerSymbol
 				&& result.charAt(4) == playerSymbol
 				&& result.charAt(8) == playerSymbol
 				|| result.charAt(2) == playerSymbol
 				&& result.charAt(4) == playerSymbol
 				&& result.charAt(6) == playerSymbol){
-			return "Player "+playerSymbol+" wins";
+			return true;
 		};
-
-		if (isItADraw(board)) {
-			return "Draw";
-		}
-		return board.showBoard();
+		return  false;
 	}
 
 	private boolean isItADraw(Board board) {
